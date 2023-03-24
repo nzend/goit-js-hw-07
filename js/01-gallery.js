d6a98cf86f8fe906ca900bcc6b7f6506f5959c19
nzend <115723110+nzend@users.noreply.github.com>
2023-03-24 19:35:38 +0200

    apdate task 1/2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,61 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItem(galleryItems);
+
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+
+// Рендер HTML галереї
+function createGalleryItem(itemsArray) {
+  const markup = itemsArray
+    .map(({ preview, original, description }) => {
+      return ` 
+     <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li> `;
+    })
+    .join("");
+  return markup;
+}
+
+// Додавання слухача подій та повернення url великого зображення
+galleryList.addEventListener("click", onGalleryListClick);
+
+function onGalleryListClick(evt) {
+  evt.preventDefault();
+  let imgUrl = "";
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  imgUrl = evt.target.dataset;
+  const instance = basicLightbox.create(
+    `
+    <div class="modal">
+        <img
+      class="gallery__image"
+      src="${imgUrl.source}"
+      width="720"
+      height="720";
+    />
+        
+        <a>Close</a>
+    </div>
+`,
+    {
+      onShow: (instance) => {
+        instance.element().querySelector("a").onclick = instance.close;
+      },
+    }
+  );
+  instance.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItem(galleryItems);
+
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+
+// Рендер HTML галереї
+function createGalleryItem(itemsArray) {
+  const markup = itemsArray
+    .map(({ preview, original, description }) => {
+      return ` 
+     <li class="gallery__item">
+  <a class="gallery__link"  href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li> `;
+    })
+    .join("");
+  return markup;
+}
+
+galleryList.addEventListener("click", onGalleryListClick);
+
+const link = document.querySelector(".gallery__link");
+console.log(link);
+
+function onGalleryListClick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+}
+const gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
